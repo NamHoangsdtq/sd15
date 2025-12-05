0d4dd4f0f24c1e053f740d0eab506dc7701f660f
thanhvinh1605 <vinhttph19362@fpt.edu.vn>
2025-12-05 23:23:37 +0700

    Update LienHe validate and fe lọc tìm kiến tt xóa

--- AppData/ViewModels/GopYRequest.cs
+++ AppData/ViewModels/GopYRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,17 @@ namespace AppData.ViewModels
 {
     public class GopYRequest
     {
+        [Required(ErrorMessage = "Họ tên không được để trống")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Họ tên phải từ 2 - 50 ký tự")]
         public string HoTen { get; set; }
+        [Required(ErrorMessage = "Email không được để trống")]
+        [EmailAddress(ErrorMessage = "Email không đúng định dạng")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Số điện thoại không được để trống")]
+        [RegularExpression(@"^(0[0-9]{9})$", ErrorMessage = "Số điện thoại phải gồm 10 số và bắt đầu bằng 0")]
         public string SoDienThoai { get; set; }
+        [Required(ErrorMessage = "Nội dung không được để trống")]
+        [MinLength(10, ErrorMessage = "Nội dung tối thiểu 10 ký tự")]
         public string NoiDung { get; set; }
 
     }
